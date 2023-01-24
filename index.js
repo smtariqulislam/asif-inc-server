d89aa3430da8ecd48d96a0b2dda59628e092a0a9
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2023-01-24 22:38:34 +0600

    finished project

--- index.js
+++ index.js
@@ -25,89 +25,69 @@ const client = new MongoClient(uri, {
 
 async function run (){
     try {
-        const usercollection = client.db("asif-inc").collection("user-info");
+      const usercollection = client.db("asif-inc").collection("user-info");
 
-        // const user = {
-        //     FirstName: 'Tanjid',
-        //     LastName : 'Islam',
-        //     Email: 'tanjid.islam.eu@gmail',
-        //     PhoneNumber: '01400740302'
+      //Read User
+      app.get("/users", async (req, res) => {
+        const cursor = usercollection.find({});
+        const users = await cursor.toArray();
+        console.log(users);
+        res.send(users);
+      });
 
-        // }
-        // const result = await usercollection.insertOne(user);
+      //Create User
+
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const result = await usercollection.insertOne(user);
+        user._id = result.insertedId;
+        res.send(user);
+      });
+
+      //single user
+      app.get("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const user = await usercollection.findOne(query);
+        res.send(user);
+      });
+
+      //Delete User
+
+      app.delete("/deleteUsers/:id", async (req, res) => {
+        const id = req.params.id;
+        // console.log(id);
+        const query = { _id: ObjectId(id) };
+        const result = await usercollection.deleteOne(query);
+        res.send(result);
+      });
+
+      // Update User
+
+      app.put("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const updated = req.body;
+        const filter = { _id: ObjectId(id) };
+
+        console.log(updated.updatedBlock);
+
+        const updateDoc = {
+          $set: {
+            FirstName: updated.FirstName,
+            LastName: updated.LastName,
+            PhoneNumber: updated.PhoneNumber,
+            block: updated.updatedBlock,
+          },
+        };
+
+        const result = await usercollection.updateOne(filter, updateDoc, {
+          new: true,
+        });
+
+        res.send(result);
 
         // console.log(result);
-
-
-
-
-        //create
-         app.get("/users", async (req, res) => {
-           const cursor = usercollection.find({});
-           const users = await cursor.toArray();
-           console.log(users);
-           res.send(users);
-         });
-
-         app.post("/users", async (req, res) => {
-           const user = req.body;
-           const result = await usercollection.insertOne(user);
-           user._id = result.insertedId;
-           res.send(user);
-         });
-
-
-
-//single user
-          app.get("/users/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const user = await usercollection.findOne(query);
-            res.send(user);
-          });
-
-
-
-          //delete
-
-          app.delete("/deleteUsers/:id", async (req, res) => {
-              const id = req.params.id;
-              // console.log(id);
-              const query = { _id: ObjectId(id) };
-              const result = await usercollection.deleteOne(query);
-              res.send(result);
-            }
-          );
-
-
-          // update 
-
-          app.put("/users/:id", async(req,res)=>{
-
-            const id = req.params.id;
-            const updated = req.body;
-            const filter = { _id: ObjectId(id) };
-
-            const updateDoc ={
-              $set:{
-               FirstName: updated.FirstName,
-               LastName: updated.LastName,
-               PhoneNumber: updated.PhoneNumber
-              },
-            }
-
-            const result = await usercollection.updateOne(filter,updateDoc)
-
-
-            res.send(result);
-            // console.log(result);
-          })
-          
-         
-
-
-
-
+      });
     }
 
     finally{
